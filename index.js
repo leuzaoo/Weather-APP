1aafe4625c7dc84fb83456857b3fa921512ef33f
leuzaoo <leuzaoocontato@gmail.com>
2022-06-01 18:03:17 -0300

    function-search

--- index.js
+++ index.js
@@ -1,2 +1,28 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
+var api = {
+    key: "d1628e7879526860e44520bd44eb7c37",
+    base: "https://api.openweathermap.org/data/2.5/",
+    lang: "pt_br",
+    units: "metric",
+};
+var city = document.querySelector(".city");
+var date = document.querySelector(".date");
+var containerImg = document.querySelector(".container-img");
+var containerTemp = document.querySelector(".container-temp");
+var tempNumber = document.querySelector(".container-temp div");
+var tempUnit = document.querySelector(".container-temp span");
+var weatherT = document.querySelector(".weather");
+var searchInput = document.querySelector(".form-control");
+var searchButton = document.querySelector(".btn");
+var lowHigh = document.querySelector(".low-high");
+searchButton.addEventListener("click", function () {
+    searchResults(searchInput.value);
+});
+searchInput.addEventListener("keypress", enter);
+function enter(event) {
+    key = event.keyCode;
+    if (key === 13) {
+        searchResults(searchInput.value);
+    }
+}
--- index.ts
+++ index.ts
@@ -1,18 +1,31 @@
+const api = {
+  key: "d1628e7879526860e44520bd44eb7c37",
+  base: "https://api.openweathermap.org/data/2.5/",
+  lang: "pt_br",
+  units: "metric",
+};
 
+const city: HTMLElement = document.querySelector(".city");
+const date: HTMLElement = document.querySelector(".date");
+const containerImg: HTMLElement = document.querySelector(".container-img");
+const containerTemp: HTMLElement = document.querySelector(".container-temp");
+const tempNumber: HTMLElement = document.querySelector(".container-temp div");
+const tempUnit: HTMLElement = document.querySelector(".container-temp span");
+const weatherT: HTMLElement = document.querySelector(".weather");
+const searchInput: HTMLInputElement = document.querySelector(".form-control");
+const searchButton: HTMLElement = document.querySelector(".btn");
+const lowHigh: HTMLElement = document.querySelector(".low-high");
 
+searchButton.addEventListener("click", function () {
+  searchResults(searchInput.value);
+});
 
-const city: HTMLElement = document.querySelector('.city');
-const date: HTMLElement = document.querySelector('.date');
-const containerImg: HTMLElement = document.querySelector('.container-img');
-const containerTemp: HTMLElement = document.querySelector('.container-temp');
-const tempNumber: HTMLElement = document.querySelector('.container-temp div');
-const tempUnit: HTMLElement = document.querySelector('.container-temp span');
-const weatherT: HTMLElement = document.querySelector('.weather');
-const searchInput: HTMLElement = document.querySelector('.form-control');
-const searchButton: HTMLElement = document.querySelector('.btn');
-const lowHigh: HTMLElement = document.querySelector('.low-high')
+searchInput.addEventListener("keypress", enter);
+function enter(event) {
+  key = event.keyCode;
+  if (key === 13) {
+    searchResults(searchInput.value);
+  }
+}
 
-
-
-
-export {}
+export {};
