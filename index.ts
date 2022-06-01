094f107e01bde3a5730cb4ef354c96cd3af34652
leuzaoo <leuzaoocontato@gmail.com>
2022-06-01 16:53:06 -0300

    ts-update

--- index.ts
+++ index.ts
@@ -1,6 +1,18 @@
 
 
 
+const city: HTMLElement = document.querySelector('.city');
+const date: HTMLElement = document.querySelector('.date');
+const containerImg: HTMLElement = document.querySelector('.container-img');
+const containerTemp: HTMLElement = document.querySelector('.container-temp');
+const tempNumber: HTMLElement = document.querySelector('.container-temp div');
+const tempUnit: HTMLElement = document.querySelector('.container-temp span');
+const weatherT: HTMLElement = document.querySelector('.weather');
+const searchInput: HTMLElement = document.querySelector('.form-control');
+const searchButton: HTMLElement = document.querySelector('.btn');
+const lowHigh: HTMLElement = document.querySelector('.low-high')
+
+
 
 
 export {}
