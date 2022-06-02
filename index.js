ea56709c1814258be6451db747fb00618469cb4c
leuzaoo <leuzaoocontato@gmail.com>
2022-06-02 15:33:23 -0300

    remove-ts

--- index.js
+++ index.js
@@ -1,139 +1,159 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var api = {
-    key: "d1628e7879526860e44520bd44eb7c37",
-    base: "https://api.openweathermap.org/data/2.5/",
-    lang: "pt_br",
-    units: "metric",
+const api = {
+  key: "64ed82577ced7f69cb1687f0ce536131",
+  base: "https://api.openweathermap.org/data/2.5/",
+  lang: "pt_br",
+  units: "metric",
 };
-var city = document.querySelector(".city");
-var date = document.querySelector(".date");
-var containerImg = document.querySelector(".container-img");
-var containerTemp = document.querySelector(".container-temp");
-var tempNumber = document.querySelector(".container-temp div");
-var tempUnit = document.querySelector(".container-temp span");
-var weatherT = document.querySelector(".weather");
-var searchInput = document.querySelector(".form-control");
-var searchButton = document.querySelector(".btn");
-var lowHigh = document.querySelector(".low-high");
-window.addEventListener("load", function () {
-    if ("geolocation" in navigator) {
-        navigator.geolocation.getCurrentPosition(setPosition, showError);
-    }
-    else {
-        alert("Este navegador não suporte Geolocalização");
-    }
-    function setPosition(position) {
-        console.log(position);
-        var lat = position.coords.latitude;
-        var long = position.coords.longitude;
-        coordResults(lat, long);
-    }
-    function showError(error) {
-        alert("erro: ".concat(error.message));
-    }
-});
+
+const city = document.querySelector(".city");
+const date = document.querySelector(".date");
+const containerImg = document.querySelector(".container-img");
+const containerTemp = document.querySelector(".container-temp");
+const tempNumber = document.querySelector(".container-temp div");
+const tempUnit = document.querySelector(".container-temp span");
+const weatherT = document.querySelector(".weather");
+const searchInput = document.querySelector(".form-control");
+const searchButton = document.querySelector(".btn");
+const lowHigh = document.querySelector(".low-high");
+
+// window.addEventListener('load', () => {
+//     //if ("geolocation" in navigator)
+//     if (navigator.geolocation) {
+//         navigator.geolocation.getCurrentPosition(setPosition, showError);
+//     }
+//     else {
+//         alert('navegador não suporta geolozalicação');
+//     }
+//     function setPosition(position) {
+//         console.log(position)
+//         let lat = position.coords.latitude;
+//         let long = position.coords.longitude;
+//         coordResults(lat, long);
+//     }
+//     function showError(error) {
+//         alert(`erro: ${error.message}`);
+//     }
+// })
+
 function coordResults(lat, long) {
-    fetch("".concat(api.base, "weather?lat=").concat(lat, "&lon=").concat(long, "&lang=").concat(api.lang, "&units=").concat(api.units, "&APPID=").concat(api.key))
-        .then(function (response) {
-        if (!response.ok) {
-            throw new Error("http error: status ".concat(response.status));
-        }
-        return response.json();
+  fetch(
+    `${api.base}weather?lat=${lat}&lon=${long}&lang=${api.lang}&units=${api.units}&APPID=${api.key}`
+  )
+    .then((response) => {
+      if (!response.ok) {
+        throw new Error(`http error: status ${response.status}`);
+      }
+      return response.json();
     })
-        .catch(function (error) {
-        alert(error.message);
+    .catch((error) => {
+      alert(error.message);
     })
-        .then(function (response) {
-        displayResults(response);
+    .then((response) => {
+      displayResults(response);
     });
 }
-function displayResults(weather) {
-    console.log(weather);
-    city.innerText = "".concat(weather.name, ", ").concat(weather.sys.country);
-    var now = new Date();
-    date.innerText = dateBuilder(now);
-    var iconName = weather.weather[0].icon;
-    containerImg.innerHTML = "<img src=\"./icons/".concat(iconName, ".png\">");
-    var temperature = "".concat(Math.round(weather.main.temp));
-    tempNumber.innerHTML = temperature;
-    tempUnit.innerHTML = "\u00B0c";
-    weatherT.innerHTML = weather.weather[0].description;
-    lowHigh.innerHTML = "".concat(Math.round(weather.main.temp_min), "\u00B0c / ").concat(Math.round(weather.main.temp_max), "\u00B0c");
-}
-function capitalizeFirstLetter(string) {
-    return string.charAt(0).toUpperCase() + string.slice(1);
-}
-function dateBuilder(d) {
-    var days = [
-        "Domingo",
-        "Segunda",
-        "Terça",
-        "Quarta",
-        "Quinta",
-        "Sexta",
-        "Sábado",
-    ];
-    var months = [
-        "Janeiro",
-        "Fevereiro",
-        "Março",
-        "Abril",
-        "Maio",
-        "Junho",
-        "Julho",
-        "Agosto",
-        "Setembro",
-        "Outubro",
-        "Novembro",
-        "Dezembro",
-    ];
-    var day = days[d.getDay()]; //getDay: 0-6
-    var date = d.getDate();
-    var month = months[d.getMonth()];
-    var year = d.getFullYear();
-    return "".concat(day, ", ").concat(date, " ").concat(month, " ").concat(year);
-}
-var tempNumberNow;
-containerTemp.addEventListener("click", changeTemp);
-function changeTemp() {
-    tempNumberNow = tempNumber.innerHTML;
-    if (tempUnit.innerHTML === "ºc") {
-        var f_1 = tempNumberNow * 1.8 + 32;
-        tempUnit.innerHTML = "ºf";
-        tempNumber.innerHTML = Math.round(f_1);
-    }
-    else {
-        var c = (tempNumberNow - 32) / 1.8;
-        tempUnit.innerHTML = "ºf";
-        tempNumber.innerHTML = Math.round(c);
-    }
-}
-function searchResults(city) {
-    fetch("".concat(api.base, "weather?q=").concat(city, "&lang=").concat(api.lang, "&units=").concat(api.units, "&APPID=").concat(api.key))
-        .then(function (response) {
-        if (!response.ok) {
-            throw new Error("http error: status ".concat(response.status));
-        }
-        return response.json();
-    })
-        .catch(function (error) {
-        alert(error.message);
-    })
-        .then(function (response) {
-        displayResults(response);
-    });
-}
+
 searchButton.addEventListener("click", function () {
-    searchResults(searchInput.value);
+  searchResults(searchInput.value);
 });
+
 searchInput.addEventListener("keypress", enter);
 function enter(event) {
-    var key = event.keyCode;
-    if (key === 13) {
-        searchResults(searchInput.value);
-    }
+  key = event.keyCode;
+  if (key === 13) {
+    searchResults(searchInput.value);
+  }
 }
-function f(f) {
-    throw new Error("Function not implemented.");
+
+function searchResults(city) {
+  fetch(
+    `${api.base}weather?q=${city}&lang=${api.lang}&units=${api.units}&APPID=${api.key}`
+  )
+    .then((response) => {
+      if (!response.ok) {
+        throw new Error(`http error: status ${response.status}`);
+      }
+      return response.json();
+    })
+    .catch((error) => {
+      alert(error.message);
+    })
+    .then((response) => {
+      displayResults(response);
+    });
+}
+
+function displayResults(weather) {
+  console.log(weather);
+
+  city.innerText = `${weather.name}, ${weather.sys.country}`;
+
+  let now = new Date();
+  date.innerText = dateBuilder(now);
+
+  let iconName = weather.weather[0].icon;
+  containerImg.innerHTML = `<img src="./icons/${iconName}.png">`;
+
+  let temperature = `${Math.round(weather.main.temp)}`;
+  tempNumber.innerHTML = temperature;
+  tempUnit.innerHTML = `°c`;
+
+  weather_tempo = weather.weather[0].description;
+  weatherT.innerText = capitalizeFirstLetter(weather_tempo);
+
+  lowHigh.innerText = `${Math.round(weather.main.temp_min)}°c / ${Math.round(
+    weather.main.temp_max
+  )}°c`;
+}
+
+function dateBuilder(d) {
+  let days = [
+    "Domingo",
+    "Segunda",
+    "Terça",
+    "Quarta",
+    "Quinta",
+    "Sexta",
+    "Sábado",
+  ];
+  let months = [
+    "Janeiro",
+    "Fevereiro",
+    "Março",
+    "Abril",
+    "Maio",
+    "Junho",
+    "Julio",
+    "Agosto",
+    "Setembro",
+    "Outubro",
+    "Novembro",
+    "Dezembro",
+  ];
+
+  let day = days[d.getDay()]; //getDay: 0-6
+  let date = d.getDate();
+  let month = months[d.getMonth()];
+  let year = d.getFullYear();
+
+  return `${day}, ${date} ${month} ${year}`;
+}
+
+containerTemp.addEventListener("click", changeTemp);
+function changeTemp() {
+  temp_number_now = tempNumber.innerHTML;
+
+  if (tempUnit.innerHTML === "°c") {
+    let f = temp_number_now * 1.8 + 32;
+    tempUnit.innerHTML = "°f";
+    tempNumber.innerHTML = Math.round(f);
+  } else {
+    let c = (temp_number_now - 32) / 1.8;
+    tempUnit.innerHTML = "°c";
+    tempNumber.innerHTML = Math.round(c);
+  }
+}
+
+function capitalizeFirstLetter(string) {
+  return string.charAt(0).toUpperCase() + string.slice(1);
 }
